999b5142bf161dc7e5da5acd65fbcb8a07782ee8
Nilsolk <awer258@gmail.com>
2025-02-10 20:17:03 +0300

    added_class_view_for_all_posts

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage
-from django.http import HttpResponse
 from .models import Post
-
-def index(request):
-    return HttpResponse("Hello world")
+from django.views.generic import ListView
 
 def post_list(request):
     post_list = Post.published.all()
@@ -28,3 +25,9 @@ def post_detail(request, year, month, day, post):
                              publish__day= day)
 
     return render(request, 'blog/post/detail.html', {'post':post})  
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/post/list.html'

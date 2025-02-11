439a960766b699d9e2afcec18b1f25028de2a584
Nilsolk <awer258@gmail.com>
2025-02-11 22:49:46 +0300

    add_filters for mane page and templatetags

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -16,6 +16,17 @@ def post_list(request):
         
     return render(request, 'blog/post/list.html', {'posts':posts})
 
+
+# def post_list(request):
+#      posts = Post.objects.all()
+#      context = {"posts": posts}
+#      return render(request,'blog/post/list.html', context)
+
+# def post_detail(request, post_id):
+#     post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
+#     context = {'post': post}
+#     return render(request, 'blog/post/detail.html', post)
+ 
 def post_detail(request, year, month, day, post):
     # post = get_object_or_404(Post, id=id, status = Post.Status.PUBLISHED, )
     post = get_object_or_404(Post, status = Post.Status.PUBLISHED, 
